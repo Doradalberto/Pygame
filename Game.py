c66e786f7bdcb65d3813487901bac453532357d2
JotaRibeiro <jota.h.ribeiro@hotmail.com>
2019-05-15 20:22:07 -0300

    Matheus

--- Game.py
+++ Game.py
@@ -420,10 +420,9 @@ try:
                     cor_usada = VERDE
                 else:
                     cor_usada = AZUL
-                    #teste
             
-            pygame.draw.line(screen, cor_usada, (xa, ya), (xb, yb), 5)
-
+            pygame.draw.line(screen, cor_usada, (xa, ya), (xb, yb), 10)
+ 
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
         
